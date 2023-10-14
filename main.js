33de5ca08196285da8cfea4b75155371ccf7633a
FlorSg <florsgroy@gmail.com>
2023-10-14 11:11:01 -0300

    clear: index.html

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-const API_KEY = "live_7OCMn01T4H1nxC3GmzgK1T64x3dwLGT5XOySTr1eFfYc1A3Dde3NNy5zhxfXshKZ";
+// const API_KEY = "live_7OCMn01T4H1nxC3GmzgK1T64x3dwLGT5XOySTr1eFfYc1A3Dde3NNy5zhxfXshKZ";
 
+// const { createApp } = Vue
+
+//   createApp({
+//     data() {
+//       return {
+//         message: 'Hello Vue!'
+//       }
+//     }
+//   }).mount('#app')
 
 
 
@@ -11,20 +20,20 @@ const API_KEY = "live_7OCMn01T4H1nxC3GmzgK1T64x3dwLGT5XOySTr1eFfYc1A3Dde3NNy5zhx
 
 
 // JavaScript para obtener imágenes de gatos desde la API
-const getCatButton = document.getElementById('getCatButton');
-const catImageContainer = document.getElementById('catImageContainer');
+// const getCatButton = document.getElementById('getCatButton');
+// const catImageContainer = document.getElementById('catImageContainer');
 
-getCatButton.addEventListener('click', () => {
-    fetch('https://api.thecatapi.com/v1/images/search')
-        .then(response => response.json())
-        .then(data => {
-            const catImageUrl = data[0].url;
-            const catImage = document.createElement('img');
-            catImage.src = catImageUrl;
-            catImage.alt = 'Imagen de un gato';
-            catImage.classList.add('cat-image');
-            catImageContainer.innerHTML = '';
-            catImageContainer.appendChild(catImage);
-        })
-        .catch(error => console.error(error));
-});
+// getCatButton.addEventListener('click', () => {
+//     fetch('https://api.thecatapi.com/v1/images/search')
+//         .then(response => response.json())
+//         .then(data => {
+//             const catImageUrl = data[0].url;
+//             const catImage = document.createElement('img');
+//             catImage.src = catImageUrl;
+//             catImage.alt = 'Imagen de un gato';
+//             catImage.classList.add('cat-image');
+//             catImageContainer.innerHTML = '';
+//             catImageContainer.appendChild(catImage);
+//         })
+//         .catch(error => console.error(error));
+// });
