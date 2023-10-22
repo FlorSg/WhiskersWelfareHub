45272d5a4a45f27114374f6df1aac772adbe90ec
FlorSg <florsgroy@gmail.com>
2023-10-21 21:53:01 -0300

    fix: bug index.html

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
-// const API_KEY = "live_7OCMn01T4H1nxC3GmzgK1T64x3dwLGT5XOySTr1eFfYc1A3Dde3NNy5zhxfXshKZ";
+const API_KEY = "live_7OCMn01T4H1nxC3GmzgK1T64x3dwLGT5XOySTr1eFfYc1A3Dde3NNy5zhxfXshKZ";
 
-// const { createApp } = Vue
+const { createApp } = Vue
 
-//   createApp({
-//     data() {
-//       return {
-//         message: 'Hello Vue!'
-//       }
-//     }
-//   }).mount('#app')
+  createApp({
+    data() {
+      return {
+        message: 'Hello Vue!',
+        url: "https://api.thecatapi.com/v1/images/search?limit=10",
+        data: [],
+        datosPaginado:[]
+      }
+    },
+    methods: {
+        fetchData(url){
+            fetch(url)
+            .then((res => res.json()
+            .then(data => {
+              console.log(data)
+                this.data = data;
+                // console.log(this.data)
+            })))
+        }
+    },
+    created() {
+        this.fetchData(this.url)
+    }
+  }).mount('#app')
 
 
 
