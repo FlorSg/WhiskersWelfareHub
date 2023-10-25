50b07858a8cdb384cd5cc3d9fa968cdf75ae0b5f
FlorSg <florsgroy@gmail.com>
2023-10-25 16:57:55 -0300

    login

--- index/script.js
+++ index/script.js
@@ -1,13 +1,105 @@
-// const username = document.getElementById('username')
-// const password = document.getElementById('password')
-// const button = document.getElementById('button')
+const wrapper = document.querySelector('.wrapper')
+const loginLink = document.querySelector('.login-link')
+const registerLink = document.querySelector('.register-link')
+const btnPopup = document.getElementById('btnlogin-popup')
+const iconClose = document.querySelector('.icon-close')
+
+registerLink.addEventListener('click', () => {
+    document.querySelector('.btnlogin-popup').innerHTML = "Registrarse";
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', () => {
+    wrapper.classList.remove('active');
+    document.querySelector('.btnlogin-popup').innerHTML = "Inicia Sesion";
+});
+
+btnPopup.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
+});
 
 
 
-// button.addEventListener('click', (e) =>{
-//     e.preventDefault()
-//     const data = {
-//         username: username.value,
-//         password: password.value
-//     }
-// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// btnOpenResponsive.addEventListener('click', () => {
+//     navbar.classList.add('active');
+// });
+
+// btnClose.addEventListener('click', () => {
+//     navbar.classList.remove('active');
+// });
