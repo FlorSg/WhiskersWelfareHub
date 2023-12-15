52553dabcc1a98d4b8bf96777dcd33e21570c045
FlorSg <florsgroy@gmail.com>
2023-12-15 15:53:27 -0300

    done: proyect final

---
+++ products/products.js
@@ -0,0 +1,30 @@
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        message: 'Hello Vue!',
+        url: "https://FlorSg.pythonanywhere.com/productos",
+        products: [],
+        datosPaginado:[]
+      }
+    },
+    methods: {
+        fetchData(url){
+            fetch(url)
+            .then((res => res.json()
+            .then(data => {
+              // console.log(data)
+                this.products = data;
+                // console.log(this.data)
+            })))
+        },
+    },
+    created() {
+        this.fetchData(this.url)
+    }
+  }).mount('#app')
+
+
+
+
